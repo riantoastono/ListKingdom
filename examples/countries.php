d018f6f12fb1aacd8a4a75721a33a3e724c95d7d
riantoastono <riantoastono@gmail.com>
2016-03-30 11:42:54 +0300

    Create countries.php

---
+++ examples/countries.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file contains examples for using the ListKingdomApi PHP-SDK.
+ *
+ * @author riantoastono <riantoastono@gmail.com>
+ * @link https://github.com/riantoastono/listkingdom
+
+ */
+ 
+// require the setup which has registered the autoloader
+require_once dirname(__FILE__) . '/setup.php';
+
+// CREATE THE ENDPOINT
+$endpoint = new ListKingdomApi_Endpoint_Countries();
+
+/*===================================================================================*/
+
+// GET ALL ITEMS
+$response = $endpoint->getCountries($pageNumber = 23, $perPage = 10);
+
+// DISPLAY RESPONSE
+echo '<pre>';
+print_r($response->body);
+echo '</pre>';
+
+/*===================================================================================*/
+
+// get country zones
+$response = $endpoint->getZones(223, $pageNumber = 1, $perPage = 10);
+
+// DISPLAY RESPONSE
+echo '<hr /><pre>';
+print_r($response->body);
+echo '</pre>';
